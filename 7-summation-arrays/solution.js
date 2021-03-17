db15fc0b2cfe35c10e1c7d16458b6c3da81978cc
David Unger <davidunger@outlook.com>
2021-03-17 09:16:31 +0100

    Execise training 7done

--- 7-summation-arrays/solution.js
+++ 7-summation-arrays/solution.js
@@ -1,8 +1,10 @@
+let sum = 0;
+
 function calculateSummation(array) {
   for (let i = 0; i < array.length; i++) {
-    const ssum = console.log(`#${i + 1} ${array[i]}`);
+    sum += array[i];
   }
-  array.reduce(reducer);
+  return sum;
 }
 
 console.log(calculateSummation([2, 4, 9, 5]));
